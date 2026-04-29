253218c300fd14a247ed356f482b892b8801c8ae
Fraser <frasermcintosh1@gmail.com>
2026-04-29 15:55:00 +1200

    Add affected tests into run tests

--- frontend/lint/module-boundaries.js
+++ frontend/lint/module-boundaries.js
@@ -261,4 +261,4 @@ function buildEnforcedRules(elements, rules) {
 
 const enforcedRules = buildEnforcedRules(elements, rules);
 
-export { elements, rules, enforcedRules };
+module.exports = { elements, rules, enforcedRules };
