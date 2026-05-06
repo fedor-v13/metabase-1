8c1aaaf8fdb2ba1e60a80fe452c61f5a90edb1e1
Ryan Laurie <iethree@gmail.com>
2026-05-06 07:33:34 -0600

    convert SearchApp to ts

--- frontend/src/metabase/search/types.ts
+++ frontend/src/metabase/search/types.ts
@@ -36,7 +36,7 @@ export type URLSearchFilterQueryParams = Partial<
   Record<FilterTypeKeys, SearchQueryParamValue>
 >;
 export type SearchAwareLocation = Location<
-  { q?: string } & URLSearchFilterQueryParams
+  { q?: string; page?: string } & URLSearchFilterQueryParams
 >;
 
 export type SearchFilters = Partial<SearchFilterPropTypes>;
